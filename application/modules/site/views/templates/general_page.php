b4ff35f12b9c1acce9e2f61f0d620459cf40cb74
Alvaro <amasitsa@live.com>
2016-01-05 09:15:02 +0300

    changes

--- application/modules/site/views/templates/general_page.php
+++ application/modules/site/views/templates/general_page.php
@@ -16,7 +16,7 @@
 	<body>
     	<!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-        <![endif]-->
+        <![en
     	<div class="body">
             <!-- Top Navigation -->
             <?php echo $this->load->view('site/includes/top_navigation', $data, TRUE); ?>
